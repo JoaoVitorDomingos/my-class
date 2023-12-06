ecba9b519bc9a1ae6db729fd4d65f361b12fd744
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-12-06 10:57:31 -0300

    media e click

--- script/script.js
+++ script/script.js
@@ -1,10 +1,29 @@
-function esqueci() {
-    alert("Escreva qualquer senha!")
+var tbody = document.getElementsByTagName("tbody")[0]
+var maxtr = tbody.childElementCount
+
+function media_notas() {
+    for(linha = 0; linha < maxtr; linha++) {
+        var tr = tbody.children[linha]
+        var nota1 = Number.parseFloat(tr.children[1].innerText)
+        var nota2 = Number.parseFloat(tr.children[2].innerText)
+        var nota3 = Number.parseFloat(tr.children[3].innerText)
+        var nota4 = Number.parseFloat(tr.children[4].innerText)
+        var media = (nota1 + nota2 + nota3 + nota4) / 4
+        var tdmedia = tr.children[5]
+        tdmedia.innerHTML = media.toFixed(1)
+    }
 }
 
-function carregar() {
-    var nome = prompt("Qual seu nome?")
-    var titulo = document.getElementById("bemvindo")
+function adicionar_click_td() {
+    for(linha = 0; linha < maxtr; linha++) {
+        var tr = tbody.children[linha]
+        for(td = 1; td > 0 && td < 5; td++) {
+            var nota = tr.children[td]
+            nota.setAttribute("onclick", "mudar_nota()")
+        }
+    }
+}
 
-    titulo.innerHTML += nome
+function mudar_nota() {
+    alert("funcionou")
 }
