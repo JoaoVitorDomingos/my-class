384bd1ee4dc6b4cc08ead859f77d3018fc155e8e
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-12-07 18:51:20 -0300

    aprovado e reprovado

--- script/script.js
+++ script/script.js
@@ -4,13 +4,17 @@ var maxtr = tbody.childElementCount
 function media_notas() {
     for(linha = 0; linha < maxtr; linha++) {
         var tr = tbody.children[linha]
+
         var nota1 = Number.parseFloat(tr.children[1].innerText)
         var nota2 = Number.parseFloat(tr.children[2].innerText)
         var nota3 = Number.parseFloat(tr.children[3].innerText)
         var nota4 = Number.parseFloat(tr.children[4].innerText)
+
         var media = (nota1 + nota2 + nota3 + nota4) / 4
         var tdmedia = tr.children[5]
         tdmedia.innerHTML = media.toFixed(1)
+
+        aprovado(linha, media)
     }
 }
 
@@ -35,4 +39,18 @@ function mudar_nota() {
     notaselecionada.innerText = Number.parseFloat(notanova)
 
     media_notas()
+}
+
+function aprovado(linha, media) {
+    var tdaprovado = tbody.children[linha].children[6]
+    console.log("tdaprovado")
+    console.log(tdaprovado)
+
+    if(media >= 6.0) {
+        tdaprovado.innerText = "Aprovado"
+        console.log("Entrou no if, média = " + media + ". Aprovado")
+    } else {
+        tdaprovado.innerText = "Reprovado"
+        console.log("Entrou no if, média = " + media + ". Reprovado")
+    }
 }
