4433aaf78b184ad0e882e3bca14ffc236680544b
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-12-07 19:43:44 -0300

    adicionar aluno

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ var tbody = document.getElementsByTagName("tbody")[0]
 var maxtr = tbody.childElementCount
 
 function media_notas() {
+    console.log('maximo de linhas = ' + maxtr)
+
     for(linha = 0; linha < maxtr; linha++) {
         var tr = tbody.children[linha]
 
@@ -43,14 +45,48 @@ function mudar_nota() {
 
 function aprovado(linha, media) {
     var tdaprovado = tbody.children[linha].children[6]
-    console.log("tdaprovado")
-    console.log(tdaprovado)
+    //console.log("tdaprovado")
+    //console.log(tdaprovado)
 
     if(media >= 6.0) {
         tdaprovado.innerText = "Aprovado"
-        console.log("Entrou no if, média = " + media + ". Aprovado")
+        //console.log("Entrou no if, média = " + media + ". Aprovado")
     } else {
         tdaprovado.innerText = "Reprovado"
-        console.log("Entrou no if, média = " + media + ". Reprovado")
+        //console.log("Entrou no if, média = " + media + ". Reprovado")
     }
+}
+
+function adicionarAluno() {
+    var nome_aluno = document.getElementById("nome-aluno")
+    var nascimento = document.getElementById("nasc-aluno")
+    
+    if(nome_aluno.value == 0 || nascimento.value == 0) {
+        alert("Preencha os campos obrigatórios: Nome do Aluno, Data de Nascimento do aluno e Sexo do Aluno")
+
+    } else {
+        alert('adicionado')
+        //console.log("Nome = " + nome_aluno.value)
+        //console.log("Nascimento = " + nascimento.value)
+
+        var linha_nova = document.createElement("tr")
+        tbody.appendChild(linha_nova)
+
+        var tdnome = document.createElement("td")
+        tdnome.innerText = nome_aluno.value
+        linha_nova.appendChild(tdnome)
+
+        for(var i = 1; i <=4; i++) {
+            var tdnota = document.createElement("td")
+            tdnota.setAttribute("onclick", "mudar_nota()")
+            linha_nova.appendChild(tdnota)
+        }
+
+        for(var j = 1; j <= 2; j++) {
+            var tdmedia_resultado = document.createElement("td")
+            linha_nova.appendChild(tdmedia_resultado)
+        }
+    }
+
+    maxtr = tbody.childElementCount
 }
